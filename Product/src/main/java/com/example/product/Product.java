2306c0afa4676d3de44b3e5b869d979bed13a44e
Amani Amdouni <101147999+amaniamdouni@users.noreply.github.com>
2024-03-18 21:43:51 +0100

    Product

---
+++ Product/src/main/java/com/example/product/Product.java
@@ -0,0 +1,20 @@
+package com.example.product;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pid;
+
+    private String pname;
+    private String pcategory;
+    private Date pdatepost = new Date(System.currentTimeMillis());
+    private String description;
+    private String pcity;
+    private double pcoin;
+}
