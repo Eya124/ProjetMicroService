c9f6a0913afca9e34e2e05e112571645d184f9cb
Amani Amdouni <101147999+amaniamdouni@users.noreply.github.com>
2024-03-18 22:25:24 +0100

    secondCommit

--- Product/src/main/java/com/example/product/ProductService.java
+++ Product/src/main/java/com/example/product/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
+@AllArgsConstructor
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
