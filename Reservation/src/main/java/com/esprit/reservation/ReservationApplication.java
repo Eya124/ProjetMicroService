301a4d53c6971d5785b428ab546134723ef21e19
Eya TRABELSI <eya.trabelsi.1@esprit.tn>
2024-03-20 19:42:51 +0100

    test

--- Reservation/src/main/java/com/esprit/reservation/ReservationApplication.java
+++ Reservation/src/main/java/com/esprit/reservation/ReservationApplication.java
@@ -2,8 +2,11 @@ package com.esprit.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+
 public class ReservationApplication {
 
     public static void main(String[] args) {
